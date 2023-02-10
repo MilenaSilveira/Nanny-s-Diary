bff8ec613d7dad2dfcae77f704eb6e6c6e2c18a2
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-02-10 11:52:27 -0500

    add child route

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+// const dashboardroutes = require('./dashboardroutes.js')
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+// router.use('/', dashboardroutes);
 
 module.exports = router;
