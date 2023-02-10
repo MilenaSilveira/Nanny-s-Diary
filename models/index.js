6165e2e4b1b7cf366464ca29347aa421b293f2ef
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-02-09 22:19:12 -0500

    Relationships

--- models/index.js
+++ models/index.js
@@ -1,15 +1,51 @@
 const Child = require('./child');
 const AdminPost = require('./adminpost');
 const Post = require('./post');
-const User = require('./user')
+const User = require('./user');
+const Admin = require('./admin');
 
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'gallery_id',
+
+User.hasMany(Child, {
+  foreignKey: 'user_id',
 });
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
+Child.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-module.exports = { Child, AdminPost, Post, User };
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.belongsTo(Admin, {
+  foreignKey: 'post_id',
+})
+
+Child.hasMany(AdminPost, {
+  foreignKey: 'child_id',
+});
+
+Admin.hasMany(AdminPost, {
+  foreignKey: 'admin_id'
+})
+
+AdminPost.belongsTo(Admin, {
+  foreignKey: 'admin_id',
+});
+
+AdminPost.belongsTo(Child, {
+  foreignKey: 'adminpost_id',
+});
+
+
+
+
+
+
+
+module.exports = { Child, AdminPost, Post, User, Admin };
